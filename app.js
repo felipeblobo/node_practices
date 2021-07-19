f2acd3ea0a73dde1926be8f9c81a796ef3d17a2a
Felipe Lobo <felipeblobo@gmail.com>
2021-07-19 15:13:04 -0300

    finalizando aula 10

--- app.js
+++ app.js
@@ -7,11 +7,16 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
   const params = queryString.parse(url.parse(req.url, true).search);
-  console.log(params);
+  let answer = '';
 
+  if(params.question == 'bestmovie') {
+    answer = 'Matrix';
+  }  else {
+    answer = 'Pergunte em outro lugar!';
+  }
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!')
+  res.end(answer)
 })
 
 server.listen(port, hostname, () => {
